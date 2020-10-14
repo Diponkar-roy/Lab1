5ec22737b0ae6e62a7a35ce7d2dc619e0a062324
Diponkar <you@example.com>
2020-10-14 12:40:30 +0600

    lab1

---
+++ first lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Diponkar");
+            Console.ReadLine();
+            //2
+
+            int x;
+            int y;
+            int sum = x + y;
+            Console.WriteLine(sum);
+            //3
+
+            int number1 = 9;
+            int number2 = 3;
+            int div = number1 / number2;
+            Console.WriteLine(div);
+
+            //4
+            Console.WeiteLine("result 1= " + result);
+            result = -5 + 8 * 6;
+            Console.WeiteLine("result 1= " + result);
+            result = (55 + 9) % 9;
+            Console.WeiteLine("result 1= " + result);
+            result = 20 + (-3 * 5 / 8 % 3);
+            Console.WeiteLine("result 1= " + result);
+            result = 5 + 15 / 3 * 2 - 8 % 3;
+        }
+    }
+}
