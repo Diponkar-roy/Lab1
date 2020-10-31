08b3c8d10dd5528e94c7e9fff48ff06b1dac9377
Diponkar <you@example.com>
2020-10-31 11:36:15 +0600

    lab3

---
+++ lab3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Triangle;
+namespace lab3
+{
+     class Student
+    {
+        String name;
+        String id;
+        String department;
+        float cgpa;
+        public void insert(String name,String id,String department,float cgpa;)
+        {
+            name= name;
+            id= id;
+            depertment= Depertment;
+            cgpa=cgpa;
+
+            Console.WriteLine(name);
+            Console.WriteLine(id);
+            Console.WriteLine(department);
+            Console.WriteLine(cgpa);
+
+            //triangle
+
+     class Triangle
+        {
+            X1=5;
+            Y1=5;
+            Z1=5;
+
+
+        }
+        }
+    }
+}
