23a8890ba07b787775c498c79412418d178ed75e
Diponkar <you@example.com>
2020-11-26 22:06:06 +0600

    Lab

---
+++ Account/Sevings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account
+{
+    class Sevings
+    {
+        public string name;
+        int accountNum;
+        public double balance = 0;
+        int trangection = 0;
+        int MinBalance = 0;
+
+        public Sevings(string Pname,double balance, int btrangection, int Abalance)
+        {
+            name = Pname;
+            balance = MinBalance;
+            trangection = btrangection;
+            MinBalance = btrangection;
+
+        }
+
+
+    }
+}
